eab24f9c375cde42127dbeb4777d197893eb218a
ueyama4508 <fe4508@yahoo.co.jp>
2023-11-10 19:45:19 +0900

    各種修正

--- js/common.js
+++ js/common.js
@@ -1,5 +1,20 @@
 
-let question = function () {
-    document.getElementById(answer )
-    console.log("Hello world!");
-  }
+function question(e) {
+    let q = document.getElementById("answer"+ e)
+    if(q.style.display == "none"){
+        q.style ="display: block;"
+    }else if(q.style.display == "block"){
+        q.style ="display: none;"
+    }else{
+    }
+    }
+
+    window.addEventListener('resize',function(){
+        let topimg = document.getElementsByClassName("topimg").src
+        if(window.innerWidth <= 450 + "px"){
+            console.log(window.innerWidth)
+            console.log(topimg)
+
+        }
+
+    })
